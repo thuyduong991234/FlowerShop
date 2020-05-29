f6e9979dc33241511fb5e8ceff4d44deff2ed041
thuyduong991234 <thuyduong991234@gmail.com>
2020-05-29 08:29:27 +0700

    search flowers follow name, color, created_at within time range

--- app/Http/Controllers/API/FlowerController.php
+++ app/Http/Controllers/API/FlowerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\FlowerRequestPatch;
 use App\Http\Requests\FlowerRequestPost;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\Flower;
@@ -13,13 +14,28 @@ class FlowerController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         //
-        $listFlowers = Flower::all();
+        $name = $request->input('name');
+        $color = $request->input('color');
+        $fromDate = $request->input('fromDate');
+        $toDate = $request->input('toDate');
+        $listFlowers = Flower::where('name','LIKE','%'.$name.'%')
+                             ->where('color','LIKE','%'.$color.'%')
+                             ->when($fromDate, function ($query, $fromDate)
+                                {
+                                    return $query->whereDate('created_at','>=',$fromDate);
+
+                                })
+                             ->when($toDate, function ($query, $toDate)
+                                {
+                                    return $query->whereDate('created_at','<=',$toDate);
+                                })
+                             ->paginate(5);
         return response($listFlowers, 200);
     }
 
@@ -31,13 +47,9 @@ class FlowerController extends Controller
      */
     public function store(FlowerRequestPost $request)
     {
-        //
-        //$request->validated();
-
         Flower::create($request->all());
 
         return response('Saved successfully', 201);
-
     }
 
     /**
@@ -46,10 +58,8 @@ class FlowerController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Flower $flower)
     {
-        //
-        $flower = Flower::where('id', '=', $id)->get();
         return response($flower, 201);
     }
 
@@ -60,12 +70,9 @@ class FlowerController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(FlowerRequestPatch $request, $id)
+    public function update(FlowerRequestPatch $request, Flower $flower)
     {
-        //
-        //$request->validated();
-
-        Flower::where('id', $id)->update($request->all());
+        $flower->update($request->all());
         return response('Updated successfully', 200);
     }
 
@@ -75,10 +82,10 @@ class FlowerController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Flower $flower)
     {
         //
-        Flower::where('id',$id)->delete();
-        return response('Deleted successfully', 200);
+        $flower->delete();
+        return response('Deleted successfully', 204);
     }
 }
